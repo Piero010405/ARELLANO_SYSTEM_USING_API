088daef5619fa6b85303fb873f52fd3852894520
Sarmiento Mr. D. (Dalessandro) <dsarmiento@arellano.pe>
2025-03-31 10:23:32 -0500

    stores

--- src/app/api/stores/[codigo]/route.js
+++ src/app/api/stores/[codigo]/route.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import { getConnection } from "@/database/connection";
 import { getIronSession } from "iron-session";
-import sql from 'mssql';
+import apiClient from "@/utils/axiosInstance";
 
 const sessionOptions = {
   password: process.env.SECRET_KEY,
@@ -13,41 +12,16 @@ export async function GET(req, { params }) {
   try {
     const res = NextResponse.json({});
     const session = await getIronSession(req, res, sessionOptions);
-
+    
     if (!session.user) {
       return NextResponse.json({ error: "No autenticado" }, { status: 401 });
     }
-
-    const storeId = params.codigo; //
-    if (!storeId) {
-      return NextResponse.json({ error: "Código de tienda requerido" }, { status: 400 });
-    }
-
-    const pool = await getConnection();
-
-    let query = `
-      SELECT TOP 1 ID.*, MUE.NOMBRE AS NOMBRE_TIENDA, MUE.[MES CONTRATO] AS MES_CONTRATO FROM INDICADORES_DEP ID
-      LEFT JOIN dbo.MUESTRA MUE ON MUE.CODIGO = ID.CODIGO
-      WHERE ID.CODIGO = @storeId
-      AND ID.PERIOD = (SELECT E2E_ID FROM NIELSSEN_CURRENT_PERIOD WHERE IS_CURRENT = 1)
-    `;
     
-    if (session.user.admin !== true) {
-      query += ` AND ID.[AS] = @nombreUsuario`;
-    }
-    
-    const result = await pool.request()
-      .input("storeId", storeId)
-      .input("nombreUsuario", sql.VarChar, session.user.name)
-      .query(query);
+    const response = await apiClient.get(`/api/stores/${params.codigo}}`);
 
-    if (result.recordset.length === 0) {
-      return NextResponse.json({ error: "Tienda no encontrada" }, { status: 404 });
-    }
-
-    return NextResponse.json(result.recordset[0], { status: 200 });
-  } catch (error) {
-    console.error("Error en la API de búsqueda por tienda:", error);
-    return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 });
+    return NextResponse.json(response.data);
+  } catch (err) {
+    console.error("Error obteniendo la tienda:", err);
+    return NextResponse.json({ error: "Error al obtener la tiendas" }, { status: 500 });
   }
 }
--- src/app/api/stores/route.js
+++ src/app/api/stores/route.js
@@ -1,94 +1,27 @@
 import { NextResponse } from "next/server";
-import { getConnection } from "@/database/connection";
 import { getIronSession } from "iron-session";
+import apiClient from "@/utils/axiosInstance";
 
 const sessionOptions = {
   password: process.env.SECRET_KEY,
   cookieName: "auth_session",
-  cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
-  },
+  cookieOptions: { secure: process.env.NODE_ENV === "production" },
 };
 
-export async function GET(req) {
+export async function GET() {
   try {
     const res = NextResponse.json({});
     const session = await getIronSession(req, res, sessionOptions);
-
+    
     if (!session.user) {
       return NextResponse.json({ error: "No autenticado" }, { status: 401 });
     }
+    
+    const response = await apiClient.get("/api/stores");
 
-    const nombreUsuario = session.user.name;
-    const pool = await getConnection();
-
-    // Obtener la paginación desde la URL
-    const { searchParams } = new URL(req.url);
-    const page = parseInt(searchParams.get("page") || "1", 10);
-    const pageSize = parseInt(searchParams.get("pageSize") || "10", 10);
-    const offset = (page - 1) * pageSize;
-
-    const query = (session.user.admin == 1) ?
-    `
-      SELECT 
-          ID.*, 
-          MUE.NOMBRE AS NOMBRE_TIENDA, 
-          MUE.[MES CONTRATO] AS MES_CONTRATO 
-      FROM INDICADORES_DEP ID
-      LEFT JOIN dbo.MUESTRA MUE 
-          ON MUE.CODIGO = ID.CODIGO
-      WHERE 
-          ID.PERIOD = (SELECT E2E_ID FROM NIELSSEN_CURRENT_PERIOD WHERE IS_CURRENT = 1)
-      ORDER BY ID.CODIGO
-      OFFSET @offset ROWS FETCH NEXT @pageSize ROWS ONLY
-    `:
-    `
-      SELECT 
-          ID.*, 
-          MUE.NOMBRE AS NOMBRE_TIENDA, 
-          MUE.[MES CONTRATO] AS MES_CONTRATO 
-      FROM INDICADORES_DEP ID
-      LEFT JOIN dbo.MUESTRA MUE 
-          ON MUE.CODIGO = ID.CODIGO
-      WHERE 
-          ID.PERIOD = (SELECT E2E_ID FROM NIELSSEN_CURRENT_PERIOD WHERE IS_CURRENT = 1)
-          AND ID.[AS] = @nombreUsuario
-      ORDER BY ID.CODIGO
-      OFFSET @offset ROWS FETCH NEXT @pageSize ROWS ONLY
-    `
-    ;
-
-    const totalQuery = (session.user.admin == 1) ?
-    `
-      SELECT COUNT(*) as total 
-      FROM INDICADORES_DEP 
-      WHERE PERIOD = (SELECT E2E_ID FROM NIELSSEN_CURRENT_PERIOD WHERE IS_CURRENT = 1) 
-    ` : 
-    `
-      SELECT COUNT(*) as total 
-      FROM INDICADORES_DEP 
-      WHERE PERIOD = (SELECT E2E_ID FROM NIELSSEN_CURRENT_PERIOD WHERE IS_CURRENT = 1) 
-      AND [AS] = @nombreUsuario
-    `
-    ;
-
-    const result = await pool.request()
-      .input("nombreUsuario", nombreUsuario)
-      .input("offset", offset)
-      .input("pageSize", pageSize)
-      .query(query);
-
-    const totalResult = await pool.request()
-      .input("nombreUsuario", nombreUsuario)
-      .query(totalQuery);
-
-    return NextResponse.json({
-      stores: result.recordset || [],
-      total: totalResult.recordset[0]?.total || 0
-    }, { status: 200 });
-
-  } catch (error) {
-    console.error("Error en la API de stores:", error);
-    return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 });
+    return NextResponse.json(response.data);
+  } catch (err) {
+    console.error("Error obteniendo las tiendas:", err);
+    return NextResponse.json({ error: "Error al obtener las tiendas" }, { status: 500 });
   }
 }
