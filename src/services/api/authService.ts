420b85a85857657ff3959a821d0121dbdd08e478
Sarmiento Mr. D. (Dalessandro) <dsarmiento@arellano.pe>
2025-08-07 17:13:06 -0500

    deleting coments

--- src/services/api/authService.ts
+++ src/services/api/authService.ts
@@ -16,7 +16,7 @@ export const authService = {
             {
                 headers: {
                     Authorization: `Bearer ${accessToken}`,
-                    Cookie: `refreshToken=${refreshToken}`, // 👈 reenviamos cookie manualmente
+                    Cookie: `refreshToken=${refreshToken}`,
                 },
                 withCredentials: true,
             }
