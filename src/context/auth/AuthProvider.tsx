6ef0da35e6829a335667ec53a50f795cad8b659c
Sarmiento Mr. D. (Dalessandro) <dsarmiento@arellano.pe>
2025-08-15 10:29:07 -0500

    VALIDATE LOG

--- src/context/auth/AuthProvider.tsx
+++ src/context/auth/AuthProvider.tsx
@@ -70,6 +70,8 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
       if (res.ok && dataValidate.user) {
         setUser(dataValidate.user);
       } else {
+        console.log("ERROR", res)
+        console.log("ERROR VALIDATE", dataValidate)
         throw new Error("No se pudo autenticar sesión");
       }
     
