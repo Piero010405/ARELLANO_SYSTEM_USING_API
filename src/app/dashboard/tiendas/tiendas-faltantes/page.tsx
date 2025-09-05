9601a3d82ebfa1777c62082b306f3de62e40a523
Piero010405 <psarmientoayala@gmail.com>
2025-09-05 14:15:44 -0500

    DINAMIC TITLE between pages

--- src/app/dashboard/tiendas/tiendas-faltantes/page.tsx
+++ src/app/dashboard/tiendas/tiendas-faltantes/page.tsx
@@ -1,28 +1,9 @@
-"use client";
+// app/dashboard/tiendas/tiendas-faltantes/page.tsx
+import { buildMetadata } from "@/app/lib/metadata";
+import PageMisTiendasFaltantes from "@/components/arellano/PageMisTiendasFaltantes";
 
-import { useAuth } from '@/hooks/useAuth';
-import StoresTablesFaltantes from "@/components/arellano/StoresTableFaltantes";
+export const metadata = buildMetadata("Mis Faltantes");
 
-function MisTiendasFaltantes() {
-  const { user } = useAuth();
-
-  if (!user) {
-    return null;
-  }
-
-  return (
-    <>
-      <div className="pb-5">
-        <h1 className="text-xl font-semibold text-gray-800 dark:text-white/90">{user && <p>❌ Mis Tiendas Faltantes</p>}</h1>
-      </div>
-
-      <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="col-span-12 space-y-6">
-          <StoresTablesFaltantes/>
-        </div>
-      </div>
-    </>
-  )
+export default function RegistrarProyeccion() {
+  return <PageMisTiendasFaltantes />;
 }
-
-export default MisTiendasFaltantes;
---
+++ src/components/arellano/PageTodasMisTiendas.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useAuth } from '@/hooks/useAuth';
+import StoresTable from "@/components/arellano/StoresTable";
+
+export default function PageTodasMisTiendas() {
+  const { user } = useAuth();
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="pb-5">
+        <h1 className="text-xl font-semibold text-gray-800 dark:text-white/90">{user && <p>🏪 Todas Mis Tiendas</p>}</h1>
+      </div>
+
+      <div className="grid grid-cols-12 gap-4 md:gap-6">
+        <div className="col-span-12 space-y-6">
+          <StoresTable pageSize={50}/>
+        </div>
+      </div>
+    </>
+  )
+}
